aee2e021a10695c07ae30717661bea3e9c4cc146
Magdalena arti Zachara <magdalena.j.zachara@gmail.com>
2019-05-04 22:48:29 +0200

    Add buttons and styles

--- js/script.js
+++ js/script.js
@@ -19,20 +19,60 @@ var Counter = React.createClass({
     });
   },
 
+  // console logs
+  getDefaultProps: function() {
+    console.log("getDefaultProps: Ustawienie wartości domyślnych");
+  },
+  componentWillMount: function() {
+    console.log(
+      "componentWillMount: Podłączenie komponentu przed wykoananiem metody 'render'"
+    );
+  },
+  componentDidMount: function() {
+    console.log(
+      "componentDidMount: Komponent zamontowany - są możliwe dalsze operacje na utworzonym elemencie"
+    );
+  },
+  componentWillReceiveProps: function() {
+    console.log(
+      "componentWillReceiveProps: Aktualizacja stanu komponentu po zmianie właściwości"
+    );
+  },
+  shouldComponentUpdate: function() {
+    console.log(
+      "shouldComponentUpdate: Sprawdzenie, czy komponent wymaga ponownego renderowania"
+    );
+  },
+  componentWillUpdate: function() {
+    console.log(
+      "componentWillUpdate: Aktualizacja komponentu, gdy metoda shouldComponentUpdate zwróci wartość 'true'"
+    );
+  },
+  componentDidUpdate: function() {
+    console.log(
+      "componentDidUpdate: Komponent uaktualniony - możliwe dalsze operacje na elemencie"
+    );
+  },
+  componentWillUnmount: function() {
+    console.log(
+      "componentWillUnmount: Usunięcie komponentu po wykonaniu 'zadania'"
+    );
+  },
+
   render: function() {
     return React.createElement(
       "div",
-      {},
+      { className: "counter" },
       React.createElement("span", {}, "Licznik " + this.state.counter),
       React.createElement(
         "button",
         { className: "buttonIncrement", onClick: this.increment },
-        "Dodaj"
+        "Dodaj 1"
       ),
       React.createElement(
         "button",
         { className: "buttonDecrement", onClick: this.decrement },
-        "Odejmij"
+        "Odejmij 1"
       )
     );
   }
